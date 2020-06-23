f0ff1cc40ca58d0ad9f12815627420f2191b488f
lsauld <you@example.com>
2020-06-23 17:38:15 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
 
     set(:views, 'app/views')
-    
+
+    get('/list_airlines') do
+        @airlines = Airline.all
+        erb :list_airlines
+    end
+
+    get('/single_airline/:id') do
+        @airline = Airline.find(params[:id])
+        @tickets_for_airline = Ticket.where(airline_id: params[:id])
+        erb :single_airline
+    end
+
+    post('/basura/:id') do 
+        remove_airline = Airline.find(params[:i$d])
+        remove_airline.destroy()
+    end
 
 end
 
